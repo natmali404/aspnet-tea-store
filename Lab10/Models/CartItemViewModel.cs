7cb3cb0ccef05baa26557ce167bec06339cec9d4
Natalia Malinowska <58657829+natmali404@users.noreply.github.com>
2025-06-07 20:24:50 +0200

    refactor: remove old files

--- Lab10/Models/CartItemViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿using Lab10.Models;
-
-public class CartItemViewModel
-{
-    public Article Article { get; set; }
-    public int Quantity { get; set; }
-    public decimal TotalPrice { get; set; }
-}
